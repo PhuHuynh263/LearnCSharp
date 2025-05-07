32918e11667478649c17651a9657d7ba06ccf2d3
PhuHuynh263 <daiphu260302@gmail.com>
2025-05-07 17:48:12 +0700

    Bai 16 Array part 1

---
+++ Hoc_C#/Bai_16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Khai báo mảng
+            int[] stt;
+            char[] ch;
+            string[] str;
+
+            //2. Khởi tạo mảng
+            stt = new int[5];
+            ch = new char[5];
+            str = new string[5];
+
+            //3. Gán giá trị cho mảng
+            int[] mang2 = new int[5] { 1, 2, 3, 4, 5 };
+
+        }
+    }
+}
